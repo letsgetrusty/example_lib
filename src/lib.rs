48b2b312d5ba64a6e7b30b16ab52366d19e8ccaf
letsgetrusty <bogdan@letsgetrusty.com>
2022-04-19 09:41:00 -0700

    init

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+
+    #[test]
+    fn test_add_with_negative_number() {
+        assert_eq!(add(-1, -2), -3);
+    }
+}
